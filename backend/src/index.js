1da7ec7c3ff527b8c174af48944ea9d8cbe15730
Ricardo Borges <ricardoborges.ufg@gmail.com>
2020-01-21 00:27:13 -0300

    change dev var and cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes.js');
+const cors = require('cors');
 
 const app = express();
 
@@ -9,6 +10,7 @@ mongoose.connect('mongodb+srv://ricardo:borges@cluster0-tyqqk.mongodb.net/test?r
     useUnifiedTopology: true
 });
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
