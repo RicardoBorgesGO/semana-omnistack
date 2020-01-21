fb6603ab14c713704ae2f78b5f286e15dc99a139
Ricardo Borges <ricardoborges.ufg@gmail.com>
2020-01-20 21:21:12 -0300

    componente, propriedade e estado

--- web/src/App.js
+++ web/src/App.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Header from './Header';
+
+// Componente: Bloco isolado de HTML, CSS e JS, o qual não interfere no restante da aplicação
+// Propriedade: Informações que um componente PAI passa para o componente FILHO
+// Estado: Informações mantidas pelo componente (Lembrar: imutabilidade)
+
 function App() {
+  const [counter, setCounter] = useState(0);
+
+  function incrementCounter() {
+    setCounter(counter+1);
+  }
+
   return (
-    <h1>Hello World</h1>
+    <>
+      <h1>Contador: {counter}</h1>
+      <button onClick={incrementCounter}>Incrementar</button>
+    </>
   );
 }
 
